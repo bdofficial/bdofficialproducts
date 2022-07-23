1ee46417987b1bed9582f5f0883ea5cb013da678
banglaploa@gmail.com <banglaploa@gmail.com>
2022-07-23 19:27:03 +0600

    hjj

--- sw.js
+++ sw.js
@@ -2,8 +2,22 @@ var cacheName = 'hello-pwa';
 var filesToCache = [
     '/',
   '/index.html',
-  '/js/new.js'
-  ];
+  '/css/bootstrap.min.css',
+  '/css/bootstrap.min.js',
+  '/css/shop/chat.css',
+  '/css/shop/style.css',
+  '/css/shop/stylepop.css',
+  '/css/shop/stylefilter.css',
+  '/css/shop/styleshop.css',
+  '/css/shop/typing.css',
+  '/js/app.js',
+  '/js/chat.js',
+  '/js/images-calc.js',
+  '/js/main.js',
+  '/js/new.js',
+  '/js/scriptfilter.js',
+  '/js/shop.js'
+    ];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
