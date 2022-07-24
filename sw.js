89037808779d806416bc13bc0d0cc286fb0424c6
banglaploa@gmail.com <banglaploa@gmail.com>
2022-07-24 14:02:09 +0600

    9999

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 var cacheName = 'hello-pwa';
 var filesToCache = [
-
+  '/',
+  '/index.html',
+  '/cstylesfilter.js'
   ];
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
