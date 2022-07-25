94eff7bbbcbdb468cadc19fc6374225000bb3f24
banglaploa@gmail.com <banglaploa@gmail.com>
2022-07-25 21:59:41 +0600

    555

---
+++ js/Chat.js
@@ -0,0 +1,44 @@
+  class InteractiveChatbox {
+    constructor(a, b, ) {
+        this.args = {
+            button: a,
+            chatbox: b
+        }
+    }
+
+    display() {
+        const {button, chatbox} = this.args;
+        
+        button.addEventListener('click', () => this.toggleState(chatbox))
+    }
+
+    toggleState(chatbox) {
+        this.state = !this.state;
+        this.showOrHideChatBox(chatbox, this.args.button);
+    }
+
+    showOrHideChatBox(chatbox, button) {
+        if(this.state) {
+            chatbox.classList.add('chatbox--active')
+            
+        } 
+        else if (!this.state) {
+          chatbox.classList.remove('chatbox--active')
+             }
+    }
+    
+}
+
+
+
+
+const chatButton = document.querySelector('.chatbox__button');
+const chatContent = document.querySelector('.chatbox__support');
+const icons = {
+    isClicked: ' ',
+    isNotClicked: ' '
+}
+const chatbox = new InteractiveChatbox(chatButton, chatContent, icons);
+chatbox.display();
+
+  
