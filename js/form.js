9fe88835b0c08d2fee4914f2cb6d2eccd45f9504
banglaploa@gmail.com <banglaploa@gmail.com>
2022-08-15 17:18:56 +0600

    555

--- js/form.js
+++ js/form.js
@@ -1,15 +1,17 @@
 let form = document.getElementById("333sb");
     form.addEventListener('submit', (e) => {
       e.preventDefault();
-      document.querySelector("#save").value = "SUBMITTING";
+      document.querySelector("#save").value = "SUBMITTED";
       let data = new FormData(form);
-      fetch('https://script.google.com/macros/s/AKfycbx-WfVTKp9gc4QE803KVeOs0lIaWclaDdq9ZgnrwNOte4hAkIi5dms9gitjOh6qdtu25Q/exec', {
+      fetch('https://script.google.com/macros/s/AKfycbxo6c78N9tATpLbbe3AxX_Rrq13SvrSizm3NVKlPqpEzHrUv23CpHhaAVWFGifQ2VE6pA/exec', {
           method: "POST",
           body: data
         })
         .then(res => res.text())
         .then(data => {
-          document.querySelector("#msg").innerHTML = data;
-          document.querySelector("#save").value = "Submit"
+          document.querySelector("#save").value = "SUBMITTED"
         });
+    document.getElementById("display_message").value='';
+    document.getElementById("message").value='';
     })
+   
