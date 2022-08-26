14f621429fe8135d958dc96b3f6f9c1cf512c5d6
banglaploa@gmail.com <banglaploa@gmail.com>
2022-08-26 12:29:43 +0600

    5

---
+++ js/SHORT/advancejavascript.js
@@ -0,0 +1,7 @@
+document.write('<div>        ');
+document.write(' <script src="./js/shop.js"> </script> ');
+document.write(' <script src="./js/scriptfilter.js"></script>	');
+document.write(' <script src="./js/popupchat.js"></script>');
+document.write('  <script src="./js/name.js"></script>');
+document.write('  <script src="./js/advancedate.js"></script>');
+document.write('</div>  ');
---
+++ js/SHORT/javascript.js
@@ -0,0 +1,7 @@
+document.write('<div>        ');
+document.write(' <script src="./js/shop.js"></script> ');
+document.write(' <script src="./js/scriptfilter.js"></script>	    ');
+document.write(' <script src="./js/popupchat.js"></script>');
+document.write('  <script src="./js/name.js"></script>');
+document.write('  <script src="./js/date.js"></script>');
+document.write('</div>  ');
