ebeb14405fde9337c06769356150bf93095d3c9e
banglaploa@gmail.com <banglaploa@gmail.com>
2022-08-27 21:31:10 +0600

    555

---
+++ token.js
@@ -0,0 +1,57 @@
+//token-save
+window.onload=function(){ 
+    window.setTimeout(function() { 
+      Verify();
+        var token = document.getElementById("token").value ;
+        //localStorage["token"] = token ;
+        localStorage.setItem("token", token) ;
+        console.log("token id saved")
+    },0000);
+};
+//save entered token
+document.getElementById("sub").addEventListener("click", function ()
+    {
+        var token = document.getElementById("token").value ;
+        //localStorage["token"] = token ;
+        localStorage.setItem("token", token) ;
+        console.log("token id saved")
+    } , false);
+//check if "token" is in localStorage
+if (localStorage["token"])
+{
+    var token = localStorage["token"] ;
+    document.getElementById("token").value = token ;
+}
+else
+{
+    document.getElementById("token").placeholder = " " ;
+    console.log("token no found in localStorage")
+}
+
+
+
+
+//Submit Button
+function Verify() {
+var token = document.getElementById("token").value;  
+  if (token == "121") {
+      $("#products").fadeOut(0)
+        .delay(0)
+        .queue(function(n) {
+          $(this).html("");
+          n();
+        }).fadeIn(0);
+window.location.replace('http://bdop.netlify.app'); 
+  }
+
+  else if (token == "143") {
+window.location.replace('http://facebook.com'); 
+   }  
+    
+else {
+  alert("Fill-Token-Correctly")
+  }            
+};
+
+
+
