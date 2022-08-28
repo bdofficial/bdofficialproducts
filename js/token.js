c8dfbc99dc44b7de4b158fb2f33c5f3b7e6d42df
banglaploa@gmail.com <banglaploa@gmail.com>
2022-08-29 01:49:38 +0600

    555

--- js/token.js
+++ js/token.js
@@ -7,12 +7,6 @@ window.onload = () =>{
         localStorage.setItem("token", token) ;
         console.log("token id saved")
     },0000);
-    //webp
-      'use strict';
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./webpsave.js');
-  }
 };
 //save entered token
 document.getElementById("sub").addEventListener("click", function ()
@@ -47,17 +41,14 @@ var token = document.getElementById("token").value;
           $(this).html("<html><style>html {background-color:black;position: absolute;left: 48%;top: 42.3%;transform: translate(-43%,-42.3%);}");
           n();
         }).fadeIn(0);
-//window.location.replace('http://bdop.netlify.app'); 
-  }
-
+window.location.replace('http://bdop.netlify.app'); 
+  } 
+ 
   else if (token == "14") {
 window.location.replace('http://bdop.netlify.app'); 
    }  
     
 else {
   alert("Fill-Token-Correctly")
-  }            
+  }      
 };
-
-
-
