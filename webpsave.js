887c2790349ac1547494da3067412320e58dd419
banglaploa@gmail.com <banglaploa@gmail.com>
2022-08-29 12:08:53 +0600

    555333

--- webpsave.js
+++ webpsave.js
@@ -7,12 +7,11 @@ self.addEventListener('activate', (e) => {
     }))
   }));
 });
-var cacheName = '77tt';
+var cacheName = '85335';
 //save cache
 var filesToCache = [
-  "/",
   "/0819746153816942409.html",
-  "/js/token.js",
+  "/js/token.js"
   ];
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
