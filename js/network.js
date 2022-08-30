25d12acd96ae0c3a0fc6dfab05aa30d597250ed0
banglaploa@gmail.com <banglaploa@gmail.com>
2022-08-30 20:41:58 +0600

    333

--- js/network.js
+++ js/network.js
@@ -1,6 +1,6 @@
 var connection = window.navigator.onLine;
 if(connection){
-window.location.replace('http://bdop.netlify.app/0819746153816942409.html');
+window.location.assign('http://bdop.netlify.app/0819746153816942409.html');
 }
 else{
   
