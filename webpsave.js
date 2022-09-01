e4b583392c16585edf316744815fcf42e6f2abd4
banglaploa@gmail.com <banglaploa@gmail.com>
2022-09-01 07:59:44 +0600

    333

--- webpsave.js
+++ webpsave.js
@@ -11,6 +11,8 @@ var cacheName = 'dj—wnjhhnn';
 //save cache
 var filesToCache = [
   "/webp.com.html",
+  "/0819746153816942409.html",
+  "/js/token.js"
   ];
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
